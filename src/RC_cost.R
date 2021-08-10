bceec6ec05e8b542dac37070d69522a6c28e6702
zhyating-n <yating.zhang@nist.gov>
2021-08-10 15:24:23 -0400

    TODO

--- src/RC_cost.R
+++ src/RC_cost.R
@@ -84,11 +84,12 @@ n_elevators = 1  # Number of elevators
 
 ############################### Calculation #####################################
 # Gravity columns
-## TODO: is the length of this vector specific to 12 story?
+## TODO: Types of steel bars used in columns
 col_s <- c(col_s3, col_s4, col_s5, col_s6, col_s7, col_s8, col_s9, col_s10, col_s11, col_s14, col_s18)
 
+## Select steel bars for columns
 selection <- function(x){
-  ## TODO: is the length of this vector specific to 12 story?
+  ## TODO: The length of this vector is not specific to 12 story.
   bar_size <- c(0.11, 0.2, 0.31, 0.44, 0.6, 0.79, 1, 1.27, 1.56, 2.25, 4)
   temp <- bar_size - x
   upper <- min(bar_size[temp > 0])
@@ -100,16 +101,16 @@ gra_concrete = (gravity$d * gravity$b / 144) * gravity$h * c1   # 8000 psi concr
 
 gra_steel = (gravity$d * gravity$b * gravity$ratio)/8  # assuming 8 steel bars per column
 gra_steel <- sapply(gra_steel, selection) * 8 * gravity$h * n_gcol  # assuming 8 steel bars per column
-  
+
 gra_tie = (gravity$d * gravity$b * gravity$sh_ratio)/n_col_tie
 gra_tie <- sapply(gra_tie, selection) * (gravity$d *6 + gravity$b * 2) /12 * gravity$h * 12 / gravity$sh_spacing * n_gcol
-  
+
 gra_total = sum(gra_concrete) + sum(gra_steel) + sum(gra_tie)
 
 
 # Perimeter columns
 col_concrete = (column$d * column$b / 144) * column$h * c1 * n_fcol   # 8000 psi concrete
-  
+
 col_in_steel = (column$d * column$b * column$in_ratio)/n_col
 col_in_steel <- sapply(col_in_steel, selection) * n_col * column$h * (n_fcol - 4)
 
@@ -126,11 +127,12 @@ col_total = sum(col_concrete) + sum(col_in_steel) + sum(col_ex_steel) + sum(col_
 
 
 # Beams
-## TODO: is the length of this vector specific to 12 story?
+## TODO: Type of steel bars used in beams
 bm_s <- c(bm_s3, bm_s4, bm_s5, bm_s6, bm_s7, bm_s8, bm_s9, bm_s10, bm_s11, bm_s14, bm_s18)
 
+## Select steel bars for beams
 selection2 <- function(x){
-  ## TODO: is the length of this vector specific to 12 story?
+  ## TODO: The length of this vector is not specific to 12 story.
   bar_size <- c(0.11, 0.2, 0.31, 0.44, 0.6, 0.79, 1, 1.27, 1.56, 2.25, 4)
   temp <- bar_size - x
   upper <- min(bar_size[temp > 0])
@@ -141,11 +143,11 @@ selection2 <- function(x){
 bm_concrete = (beam$d * beam$b / 144) * beam$l * c2   # 5000 psi concrete
 
 bm_top_steel = (beam$d * beam$b * beam$top_ratio) / n_bm_top
-bm_top_steel <- sapply(bm_top_steel, selection2) * n_bm_top * beam$l 
-  
+bm_top_steel <- sapply(bm_top_steel, selection2) * n_bm_top * beam$l
+
 bm_btm_steel = (beam$d * beam$b * beam$btm_ratio) / n_bm_btm
-bm_btm_steel <- sapply(bm_btm_steel, selection2) * n_bm_btm * beam$l 
-  
+bm_btm_steel <- sapply(bm_btm_steel, selection2) * n_bm_btm * beam$l
+
 bm_stirrup = (beam$d * beam$b * beam$sh_ratio) / n_bm_stirrup
 bm_stirrup <- sapply(bm_stirrup, selection2) * (beam$d * 6 + beam$b * 2) /12 * beam$l * 12 / beam$sh_spacing
 
@@ -157,7 +159,7 @@ slab = (slab$d / 12) * area * slab_price
 slab_total = sum(slab)
 
 # Exterior walls
-wall_total = (H * l_bm - n_story * n_windows * window_area) * wall_price  
+wall_total = (H * l_bm - n_story * n_windows * window_area) * wall_price
 
 # Exterior windows
 window_total = n_story * n_windows * (window_glass_price + window_frame_price)
